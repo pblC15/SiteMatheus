ca26bc1827449233da2bc4df0ea652997ca0b972
Pablo Cassiano <pabloboyreis@hotmail.com>
2021-05-26 10:20:00 -0300

    Finalizando Animações HOME e SOBRE

--- js/script.js
+++ js/script.js
@@ -5,4 +5,28 @@ $(function() {
         $(".menuMobile-content").slideToggle();
     });
 
+
+    //Animação 
+    var $target = $('.anime'),
+        animationClass = 'anime-start',
+        offset = $(window).height() * 3 / 4;
+
+    function animeScroll2() {
+        var documentTop = $(document).scrollTop();
+
+        $target.each(function() {
+            var itemTop = $(this).offset().top;
+
+            if (documentTop > itemTop - offset) {
+                $(this).addClass(animationClass);
+            }
+        });
+    }
+
+    animeScroll2();
+
+    $(document).scroll(function() {
+        animeScroll2();
+    });
+
 });
